09c63b283f711a45f614994c3484b9bf458dcbfc
Jay Nayak <jayknayak@gmail.com>
2024-08-22 14:13:31 -0700

    From windowsOM add code solution.

---
+++ String/hr_merge_the_tools.py
@@ -0,0 +1,18 @@
+def merge_the_tools(string, k):
+    # your code goes here
+    substr_count = int(len(string) / k)
+
+    j = 0
+    for i in range(0, substr_count):
+        substr = string[j:j + k]
+        result_str = ''
+        for z in range(0, len(substr)):
+            if substr[z] not in result_str:
+                result_str = result_str + substr[z]
+        print(result_str)
+        j = j + k
+
+
+
+string, k = 'AABCAAADA', 3
+merge_the_tools(string, k)
